600ce8c99e39aac53390f9e7927ab9b4a82330a7
Kevin Parsons <kevpar@microsoft.com>
2023-11-04 01:20:42 -0700

    Initial

---
+++ commands.go
@@ -0,0 +1,396 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"syscall"
+	"time"
+
+	"github.com/kevpar/hcstool/internal/hcsschema"
+	"github.com/kevpar/hcstool/internal/vmcompute"
+	"github.com/kevpar/repl-go"
+	"golang.org/x/sys/windows"
+)
+
+func allCommands() []repl.Command[*state] {
+	return []repl.Command[*state]{
+		&createCommand{},
+		&startCommand{},
+		&closeCommand{},
+		&suspendCommand{},
+		&resumeCommand{},
+		&saveCommand{},
+		&propsCommand{},
+		&grantCommand{},
+		&defaultCommand{},
+		&listCommand{},
+	}
+}
+
+type state struct {
+	def     string
+	systems map[string]*cs
+}
+
+type cs struct {
+	handle   vmcompute.HcsSystem
+	callback vmcompute.HcsCallback
+	ch       chan *hcsNotification
+}
+
+func setupCommonFlags(cf *commonFlags, fs *flag.FlagSet) {
+	cf.cs = fs.String("cs", "", "Specifies the compute system to operate on.")
+}
+
+type commonFlags struct {
+	cs *string
+}
+
+func getCS(state *state, cf *commonFlags) (string, *cs, error) {
+	key := state.def
+	if cs := *cf.cs; cs != "" {
+		key = cs
+	}
+	if key == "" {
+		return "", nil, fmt.Errorf("must specify a default compute system or use -cs flag")
+	}
+	cs, ok := state.systems[key]
+	if !ok {
+		return "", nil, fmt.Errorf("compute system not opened: %s", key)
+	}
+	return key, cs, nil
+}
+
+type createCommand struct{}
+
+func (c *createCommand) Name() string                { return "create" }
+func (c *createCommand) Description() string         { return "Creates a compute system." }
+func (c *createCommand) ArgHelp() string             { return "ID PATH" }
+func (c *createCommand) SetupFlags(fs *flag.FlagSet) {}
+
+func (c *createCommand) Execute(state *state, fs *flag.FlagSet) error {
+	id := fs.Arg(0)
+	if _, ok := state.systems[id]; ok {
+		return fmt.Errorf("compute system already exists: %s", id)
+	}
+	var cs cs
+	doc, err := os.ReadFile(fs.Arg(1))
+	if err != nil {
+		return err
+	}
+	var result *uint16
+	if err := vmcompute.HcsCreateComputeSystem(fs.Arg(0), string(doc), 0, &cs.handle, &result); err != nil && err != vmcompute.ErrVmcomputeOperationPending {
+		return err
+	}
+	if err := vmcompute.HcsRegisterComputeSystemCallback(cs.handle, syscall.NewCallback(computeSystemCallback), 0, &cs.callback); err != nil {
+		return err
+	}
+	cs.ch = make(chan *hcsNotification)
+	registerNotification(0, cs.ch)
+	if err := func() error {
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		n, err := pumpNotificationsUntil(ctx, cs.ch, 2)
+		if err != nil {
+			return err
+		}
+		if n.notificationStatus != 0 {
+			return fmt.Errorf("%s", stringNotification(n))
+		}
+		return nil
+	}(); err != nil {
+		return err
+	}
+	state.systems[id] = &cs
+	return nil
+}
+
+type startCommand struct{ cf commonFlags }
+
+func (c *startCommand) Name() string                { return "start" }
+func (c *startCommand) Description() string         { return "Starts a compute system." }
+func (c *startCommand) ArgHelp() string             { return "" }
+func (c *startCommand) SetupFlags(fs *flag.FlagSet) { setupCommonFlags(&c.cf, fs) }
+
+func (c *startCommand) Execute(state *state, fs *flag.FlagSet) error {
+	_, cs, err := getCS(state, &c.cf)
+	if err != nil {
+		return err
+	}
+	var result *uint16
+	if err := vmcompute.HcsStartComputeSystem(cs.handle, "", &result); err != nil && err != vmcompute.ErrVmcomputeOperationPending {
+		return err
+	}
+	if err := func() error {
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		n, err := pumpNotificationsUntil(ctx, cs.ch, 3)
+		if err != nil {
+			return err
+		}
+		if n.notificationStatus != 0 {
+			return fmt.Errorf("%s", stringNotification(n))
+		}
+		return nil
+	}(); err != nil {
+		return err
+	}
+	return nil
+}
+
+type closeCommand struct{ cf commonFlags }
+
+func (c *closeCommand) Name() string                { return "close" }
+func (c *closeCommand) Description() string         { return "Closes a compute system." }
+func (c *closeCommand) ArgHelp() string             { return "" }
+func (c *closeCommand) SetupFlags(fs *flag.FlagSet) { setupCommonFlags(&c.cf, fs) }
+
+func (c *closeCommand) Execute(state *state, fs *flag.FlagSet) error {
+	id, cs, err := getCS(state, &c.cf)
+	if err != nil {
+		return err
+	}
+	if err := vmcompute.HcsUnregisterComputeSystemCallback(cs.callback); err != nil {
+		return err
+	}
+	cs.callback = 0
+	if err := vmcompute.HcsCloseComputeSystem(cs.handle); err != nil {
+		return err
+	}
+	cs.handle = 0
+	delete(state.systems, id)
+	if state.def == id {
+		state.def = ""
+	}
+	return nil
+}
+
+type suspendCommand struct{ cf commonFlags }
+
+func (c *suspendCommand) Name() string                { return "pause" }
+func (c *suspendCommand) Description() string         { return "Pauses a compute system." }
+func (c *suspendCommand) ArgHelp() string             { return "" }
+func (c *suspendCommand) SetupFlags(fs *flag.FlagSet) { setupCommonFlags(&c.cf, fs) }
+
+func (c *suspendCommand) Execute(state *state, fs *flag.FlagSet) error {
+	_, cs, err := getCS(state, &c.cf)
+	if err != nil {
+		return err
+	}
+	var result *uint16
+	if err := vmcompute.HcsPauseComputeSystem(cs.handle, "", &result); err != nil && err != vmcompute.ErrVmcomputeOperationPending {
+		return err
+	}
+	if err := func() error {
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		n, err := pumpNotificationsUntil(ctx, cs.ch, 4)
+		if err != nil {
+			return err
+		}
+		if n.notificationStatus != 0 {
+			return fmt.Errorf("%s", stringNotification(n))
+		}
+		return nil
+	}(); err != nil {
+		return err
+	}
+	return nil
+}
+
+type resumeCommand struct{ cf commonFlags }
+
+func (c *resumeCommand) Name() string                { return "resume" }
+func (c *resumeCommand) Description() string         { return "Resumes a compute system." }
+func (c *resumeCommand) ArgHelp() string             { return "" }
+func (c *resumeCommand) SetupFlags(fs *flag.FlagSet) { setupCommonFlags(&c.cf, fs) }
+
+func (c *resumeCommand) Execute(state *state, fs *flag.FlagSet) error {
+	_, cs, err := getCS(state, &c.cf)
+	if err != nil {
+		return err
+	}
+	var result *uint16
+	if err := vmcompute.HcsResumeComputeSystem(cs.handle, "", &result); err != nil && err != vmcompute.ErrVmcomputeOperationPending {
+		return err
+	}
+	if err := func() error {
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		n, err := pumpNotificationsUntil(ctx, cs.ch, 5)
+		if err != nil {
+			return err
+		}
+		if n.notificationStatus != 0 {
+			return fmt.Errorf("%s", stringNotification(n))
+		}
+		return nil
+	}(); err != nil {
+		return err
+	}
+	return nil
+}
+
+type saveCommand struct{ cf commonFlags }
+
+func (c *saveCommand) Name() string                { return "save" }
+func (c *saveCommand) Description() string         { return "Saves the compute system to disk." }
+func (c *saveCommand) ArgHelp() string             { return "PATH" }
+func (c *saveCommand) SetupFlags(fs *flag.FlagSet) { setupCommonFlags(&c.cf, fs) }
+
+func (c *saveCommand) Execute(state *state, fs *flag.FlagSet) error {
+	_, cs, err := getCS(state, &c.cf)
+	if err != nil {
+		return err
+	}
+	path := fs.Arg(0)
+	path, err = filepath.Abs(path)
+	if err != nil {
+		return err
+	}
+	if err := os.MkdirAll(filepath.Dir(path), 0755); err != nil {
+		return err
+	}
+	so := hcsschema.SaveOptions{
+		SaveType:          "ToFile",
+		SaveStateFilePath: path,
+	}
+	j, err := json.Marshal(so)
+	if err != nil {
+		return err
+	}
+	var result *uint16
+	if err := vmcompute.HcsSaveComputeSystem(cs.handle, string(j), &result); err != nil && err != vmcompute.ErrVmcomputeOperationPending {
+		return err
+	}
+	if err := func() error {
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		n, err := pumpNotificationsUntil(ctx, cs.ch, 8)
+		if err != nil {
+			return err
+		}
+		if n.notificationStatus != 0 {
+			return fmt.Errorf("%s", stringNotification(n))
+		}
+		return nil
+	}(); err != nil {
+		return err
+	}
+	return nil
+}
+
+type propsCommand struct{ cf commonFlags }
+
+func (c *propsCommand) Name() string                { return "props" }
+func (c *propsCommand) Description() string         { return "Lists compute system properties." }
+func (c *propsCommand) ArgHelp() string             { return "" }
+func (c *propsCommand) SetupFlags(fs *flag.FlagSet) { setupCommonFlags(&c.cf, fs) }
+
+func (c *propsCommand) Execute(state *state, fs *flag.FlagSet) error {
+	_, cs, err := getCS(state, &c.cf)
+	if err != nil {
+		return err
+	}
+	pq := hcsschema.PropertyQuery{
+		Queries: map[string]interface{}{
+			"Basic":     nil,
+			"VmVersion": nil,
+		},
+	}
+	j, err := json.Marshal(pq)
+	if err != nil {
+		return err
+	}
+	var (
+		properties *uint16
+		result     *uint16
+	)
+	if err := vmcompute.HcsGetComputeSystemProperties(cs.handle, string(j), &properties, &result); err != nil {
+		return err
+	}
+	var props struct {
+		PropertyResponses struct {
+			Basic struct {
+				Response struct {
+					RuntimeId string
+				}
+			}
+			VmVersion struct {
+				Response struct {
+					Major uint
+					Minor uint
+				}
+			}
+		}
+	}
+	if err := json.Unmarshal([]byte(windows.UTF16PtrToString(properties)), &props); err != nil {
+		return err
+	}
+	fmt.Printf("RuntimeID: %s\n", props.PropertyResponses.Basic.Response.RuntimeId)
+	fmt.Printf("VmVersion: %d.%d\n", props.PropertyResponses.VmVersion.Response.Major, props.PropertyResponses.VmVersion.Response.Minor)
+	return nil
+}
+
+type grantCommand struct{}
+
+func (c *grantCommand) Name() string { return "grant" }
+func (c *grantCommand) Description() string {
+	return "Grants a given compute system ID access to a file."
+}
+func (c *grantCommand) ArgHelp() string             { return "ID PATH" }
+func (c *grantCommand) SetupFlags(fs *flag.FlagSet) {}
+
+func (c *grantCommand) Execute(state *state, fs *flag.FlagSet) error {
+	path := fs.Arg(1)
+	path, err := filepath.Abs(path)
+	if err != nil {
+		return err
+	}
+	if err := vmcompute.GrantVmAccess(fs.Arg(0), path); err != nil {
+		return err
+	}
+	return nil
+}
+
+type defaultCommand struct{ unset *bool }
+
+func (c *defaultCommand) Name() string { return "default" }
+func (c *defaultCommand) Description() string {
+	return "Sets the default compute system to operate on."
+}
+func (c *defaultCommand) ArgHelp() string { return "ID" }
+func (c *defaultCommand) SetupFlags(fs *flag.FlagSet) {
+	c.unset = fs.Bool("unset", false, "Set the default to nothing.")
+}
+
+func (c *defaultCommand) Execute(state *state, fs *flag.FlagSet) error {
+	if *c.unset {
+		state.def = ""
+		return nil
+	}
+	id := fs.Arg(0)
+	if _, ok := state.systems[id]; !ok {
+		return fmt.Errorf("compute system not found: %s", id)
+	}
+	state.def = id
+	return nil
+}
+
+type listCommand struct{}
+
+func (c *listCommand) Name() string                { return "list" }
+func (c *listCommand) Description() string         { return "Lists compute systems." }
+func (c *listCommand) ArgHelp() string             { return "" }
+func (c *listCommand) SetupFlags(fs *flag.FlagSet) {}
+
+func (c *listCommand) Execute(state *state, fs *flag.FlagSet) error {
+	for id := range state.systems {
+		fmt.Printf("%s\n", id)
+	}
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/kevpar/hcstool
+
+go 1.20
+
+require (
+	github.com/kevpar/repl-go v0.0.0-20231030174411-ed82b598138e
+	golang.org/x/sys v0.13.0
+)
